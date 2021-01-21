94c9bb22d22e16295c56572b9cfde6807addf558
Darkel Zezrucion <kelvinjune2@gmail.com>
2021-01-21 07:28:58 +0100

    First commit

---
+++ src/emails/account.js
@@ -0,0 +1,41 @@
+const nodemailer = require('nodemailer');
+
+const user = 'noreply@swiftcryptohub.com';
+const pass = process.env.MAIL_PASSWORD;
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+  host: 'mail.swiftcryptohub.com',
+  port: 26,
+  secure: false, // true for 465, false for other ports
+  auth: {user, pass},
+  tls: {
+    rejectUnauthorized: false
+  }
+});
+
+const sender = async (to, subject, text) => {
+  const from = 'noreply@swiftcryptohub.com';
+  await transporter.sendMail({
+    from, to, subject, text
+  });
+}
+
+const sendWelcomeMail = async (email, name) => {
+  const subject = "Thanks for joining in!";
+  const text = `Welcome to the app, ${ name }. let me know how you get along with the app.`;
+
+  await sender(email, subject, text);
+}
+
+const sendCancellationMail = async (email, name) => {
+  const subject = "Sorry to see you go";
+  const text = `Goodbye, ${ name }. I hope to see you back sometime soon`;
+
+  await sender(email, subject, text);
+}
+
+module.exports = {
+  sendWelcomeMail,
+  sendCancellationMail
+}
